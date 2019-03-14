cb9be9659f8343acf385f465ea4cb42b68366e6a
minhkies <minhkies@gmail.com>
2019-03-14 01:00:49 -0700

    2nd commit

--- js/app.js
+++ js/app.js
@@ -1,12 +1,50 @@
-(function () {
+(function() {
 
     var myNavIcon = document.querySelector('.mobile-menu');
     var myMainMenu = document.querySelector('.menu');
 
-    myNavIcon.addEventListener('click', function (event) {
+    myNavIcon.addEventListener('click', function(event) {
         event.preventDefault();
         myMainMenu.classList.toggle('show');
 
     }, false);
 
 })();
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+};
+
+document.getElementById("defaultOpen").click();
+
+function openCity(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tab-links");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
